3fd61a5f80c116c3fb2d34e8c2e30ff4fcf5967e
Ian-Mc-Okeyo <ianokeyo14559@gmail.com>
2024-12-19 00:59:32 +0300

    update church locations

--- .tmp/temp_app.py
+++ .tmp/temp_app.py
@@ -35,6 +35,9 @@ class Church(db.Model):
     long = db.Column(db.Float, nullable=False)
     county = db.Column(db.String(100))
     ward = db.Column(db.String(100))
+    address = db.Column(db.String(200), nullable = True)
+    phone = db.Column(db.String(100), nullable = True)
+    members = db.Column(db.Integer, nullable = True)
 
     def to_dict(self):
         return {
@@ -44,8 +47,69 @@ class Church(db.Model):
             'lng': self.long,
             'county': self.county,
             'ward': self.ward,
+            'address': self.address,
+            'phone': self.phone,
+            'members': self.members,
             'type': 'church'  
         }
+
+class ProposedChurchUpdate(db.Model):
+    __tablename__ = 'church_update'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    church_id = db.Column(db.Integer)
+    name = db.Column(db.String(255), nullable=False)
+    lat = db.Column(db.Float, nullable=False)
+    long = db.Column(db.Float, nullable=False)
+    county = db.Column(db.String(100))
+    ward = db.Column(db.String(100))
+    address = db.Column(db.String(200), nullable = True)
+    phone = db.Column(db.String(100), nullable = True)
+    members = db.Column(db.Integer, nullable = True)
+    approval_status = db.Column(db.Integer, default = 0) # 0 - pending, 1 - approved
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'church_id': self.church_id,
+            'name': self.name,
+            'lat': self.lat,
+            'lng': self.long,
+            'county': self.county,
+            'ward': self.ward,
+            'address': self.address,
+            'phone': self.phone,
+            'members': self.members,
+            'type': 'church'  
+        }
+
+# # Function to load data from CSV and insert into the database
+# def load_church_data_from_csv(csv_file):
+#     # Read the CSV file into a DataFrame
+#     df = pd.read_csv(csv_file)
+
+#     # Iterate through each row in the DataFrame and add it to the database
+#     with app.app_context():  # Ensure the Flask app context is available
+#         for index, row in df.iterrows():
+#             church = Church(
+#                 id=row['id'], 
+#                 name=row['Name'], 
+#                 lat=row['Lat'], 
+#                 long=row['Long'], 
+#                 county=row['County'], 
+#                 ward=row['Ward']
+#             )
+#             db.session.add(church)
+        
+#         # Commit all the changes to the database
+#         db.session.commit()
+#         print(f"{len(df)} records inserted into the database.")
+
+# load_church_data_from_csv("../SDA_KENYA.csv")
+
+# Call the function with your CSV file
+# load_church_data_from_csv('church_data.csv')
+
 
 @app.route('/')
 def index():
@@ -86,6 +150,33 @@ def search_locations():
         return jsonify({'locations': locations})
     except Exception as e:
         return jsonify({'error': str(e)}), 500
+
+@app.route('/api/church/update', methods=['GET', 'POST', 'PUT'])
+def update_church_details():
+    if request.method == 'PUT':
+        data = request.json
+        print(data)
+
+        # This is temporary. To be approved by an admin before it's applied
+        church = Church.query.filter_by(id = data['id']).first()
+        if not church:
+            return jsonify({'error': 'Church not found'}), 404
+        church.name = data['name']
+        church.lat = data['lat']
+        church.long = data['lng']
+        church.members = data['members']
+        church.phone = data['phone']
+        church.address = data['address']
+        church.approval_status = 0 # pending state
+
+        # create a proposed change that will be applied later
+        new_proposed_church_update = ProposedChurchUpdate(church_id = data['id'], name=data['name'], lat=data['lat'], long=data['lng'],
+                                                          members = data['members'], phone=data['phone'], address=data['address'], county=church.county,
+                                                          ward=church.ward)
+        db.session.add(new_proposed_church_update)
+
+        db.session.commit()
+        return jsonify({'message': "success"}), 201
 
 if __name__ == '__main__':
     app.run(debug=True, port=8000)
